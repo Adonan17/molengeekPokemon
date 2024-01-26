10b999e70796aadc155f4e76587a23e08c94436a
Adonan17 <adnaneouassari@gmail.com>
2024-01-26 23:33:37 +0100

    some design changes

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,12 +1,9 @@
-export let letsPlayBtn = document.querySelector('#letsPlayBtn');
+export let letsPlayBtn = document.querySelector('#startMenuBtn');
 export let pokedex = document.querySelector('#pokedex');
 
 export function setupMenu(){
     letsPlayBtn.addEventListener('click', () =>{
-        letsPlayBtn.style.display = 'none'
-        pokedex.style.display = 'flex'
+        document.querySelector('#startGame').style.display = 'none';
+        pokedex.style.display = 'flex';
     })
-
-
 }
-    
